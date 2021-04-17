9ad43d7c46eea3e0938dba6f5447511bfecf57a6
Maisy Rahmawati <maisy.rahmawati@gmail.com>
2021-04-17 14:31:37 +0700

    feat: add service, serviceImpl, and repository for Order v1

---
+++ sipel/src/main/java/propen/impl/sipel/service/OrderServiceImpl.java
@@ -0,0 +1,39 @@
+package propen.impl.sipel.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import propen.impl.sipel.model.OrderModel;
+import propen.impl.sipel.repository.OrderDb;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class OrderServiceImpl implements OrderService {
+    @Autowired
+    OrderDb orderDb;
+
+    @Override
+    public void addOrder(OrderModel order) {
+        orderDb.save(order);
+    }
+
+    @Override
+    public OrderModel updateOrder(OrderModel orderModel) {
+        orderDb.save(orderModel);
+
+        return orderModel;
+    }
+
+    @Override
+    public List<OrderModel> getOrderList() {
+        return orderDb.findAll();
+    }
+
+    @Override
+    public Optional<OrderModel> getOrderById(Long idOrder) {
+        return orderDb.findById(idOrder);
+    }
+}
