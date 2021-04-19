87d125efbf576551c3d2c277b873b77f0207a3db
hanna_jtm <hanna14jtm@gmail.com>
2021-04-20 01:10:42 +0700

    feat: penugasan engineer containers bug fixed

--- sipel-frontend/src/containers/PenugasanEngineer/index.js
+++ sipel-frontend/src/containers/PenugasanEngineer/index.js
@@ -7,8 +7,8 @@ class PenugasanEngineer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            ordersVerified: [ {items : [10,  '000010/I/PO-LA/2021', 'PIMS-190621/PT.Aplikasinusa', 'Project Installation (PI), Managed Service (MS)', 'Belum ditugaskan', 'Belum ditugaskan', 'button perbarui']},
-            {items : [9,  '000010/I/PO-LA/2021', 'PIMS-190621/PT.Aplikasinusa', 'Project Installation (PI), Managed Service (MS)', 'Belum ditugaskan', 'Belum ditugaskan', 'button perbarui']}],
+            ordersVerified: [ {items : [10,  '000010/I/PO-LA/2021', 'PIMS-190621/PT.Aplikasinusa', 'Project Installation (PI), Managed Service (MS)', 'Belum ditugaskan', 'Belum ditugaskan']},
+            {items : [9,  '000010/I/PO-LA/2021', 'PIMS-190621/PT.Aplikasinusa', 'Project Installation (PI), Managed Service (MS)', 'Belum ditugaskan', 'Belum ditugaskan']}],
             isLoading: false,
         }; 
     }
@@ -31,16 +31,15 @@ class PenugasanEngineer extends Component {
         const { ordersVerified} = this.state;
         const tableHeaders = ['No.', 'Id Order', 'Nomor PO', 'Nama Order', 'Tipe', 'PIC PI', 'PIC MS', 'Aksi'];
 
-        ordersVerified.forEach((order) =>
-            {console.log(order)
-                // order.map((items) => items.push(<CustomizedButtons variant="contained" size="small" color="#FD693E">perbarui</CustomizedButtons>))
-            });
+        ordersVerified.forEach((order) => {
+            order.items.push(<CustomizedButtons variant="contained" size="small" color="#FD693E">perbarui</CustomizedButtons>);
+            order.items.splice(7, 1);
+        });
 
         return (
             <div>
                 <h1>Daftar Order</h1>
                 <CustomizedTables headers={tableHeaders} rows={ordersVerified}/>
-                <CustomizedButtons variant="contained" size="small" color="#FD693E">perbarui</CustomizedButtons>
             </div>
         );
     }
