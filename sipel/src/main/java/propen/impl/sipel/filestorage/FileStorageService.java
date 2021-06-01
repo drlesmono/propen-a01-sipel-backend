0e24708328ca2be8240d70d42020f1b278d6f80a
hannajtm <hanna14jtm@gmail.com>
2021-06-01 21:06:54 +0700

    file storage update code

--- sipel/src/main/java/propen/impl/sipel/filestorage/FileStorageService.java
+++ sipel/src/main/java/propen/impl/sipel/filestorage/FileStorageService.java
@@ -17,27 +17,33 @@ import java.nio.file.StandardCopyOption;
 @Service
 public class FileStorageService {
 
-    private final Path fileStorageLocation;
+//    private final Path fileStorageLocation;
+
+    private static String UPLOADED_FOLDER = System.getProperty("java.io.tmpdir");
 
     // constructor
     // pembuatan directory untuk menyimpan file di local server
-    @Autowired
-    public FileStorageService(FileStorageProperties fileStorageProperties) {
-        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir()).toAbsolutePath();
-
-        try{
-            Files.createDirectories(this.fileStorageLocation);
-        }catch (Exception e){
-            throw new FileStorageException("Directory untuk upload gagal dibuat");
-        }
-    }
+//    @Autowired
+//    public FileStorageService(FileStorageProperties fileStorageProperties) {
+//        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir()).toAbsolutePath();
+//
+//        try{
+//            Files.createDirectories(this.fileStorageLocation);
+//        }catch (Exception e){
+//            throw new FileStorageException("Directory untuk upload gagal dibuat");
+//        }
+//    }
 
     //  Menyimpan file ke local server
     public String storeFile(MultipartFile file, String fileNameTarget){
         String fileName = fileNameTarget;
         try{
-           Path targetLocation = this.fileStorageLocation.resolve(fileName);
-           Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+//           Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            byte[] bytes = file.getBytes();
+//            Path targetLocation = Paths.get(UPLOADED_FOLDER + fileName);
+            Path targetLocation = getFilePath(fileName);
+//           Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            Files.write(targetLocation, bytes);
            return fileName;
         }catch (IOException e){
             throw new FileStorageException("File "+fileName+" gagal disimpan, silahkan coba lagi!", e);
@@ -46,13 +52,15 @@ public class FileStorageService {
 
     // Mengambil file path
     public Path getFilePath(String fileName){
-        return this.fileStorageLocation.resolve(fileName).normalize();
+//        return this.fileStorageLocation.resolve(fileName).normalize();
+        return Paths.get(UPLOADED_FOLDER + fileName);
     }
 
     // Mengakses file dari local server
     public Resource loadFileAsResource(String fileName){
         try{
             Path filePath = getFilePath(fileName);
+//            Path filePath = Paths.get(UPLOADED_FOLDER + fileName);
             Resource resource = new UrlResource(filePath.toUri());
             if(resource.exists()){
                 return resource;
