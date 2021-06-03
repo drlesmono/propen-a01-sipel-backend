31c0163836c1ec18d1b10f5b33d925c428e73002
hannajtm <hanna14jtm@gmail.com>
2021-06-03 12:19:50 +0700

    feat: preview and download fixed

--- sipel/src/main/java/propen/impl/sipel/filestorage/FileStorageService.java
+++ sipel/src/main/java/propen/impl/sipel/filestorage/FileStorageService.java
@@ -22,34 +22,11 @@ import java.net.URI;
 @Service
 public class FileStorageService {
 
-//    private final Path fileStorageLocation;
-
-    private static String UPLOADED_FOLDER = System.getProperty("java.io.tmpdir");
-
-    // constructor
-    // pembuatan directory untuk menyimpan file di local server
-//    @Autowired
-//    public FileStorageService(FileStorageProperties fileStorageProperties) {
-//        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir()).toAbsolutePath();
-//
-//        try{
-//            Files.createDirectories(this.fileStorageLocation);
-//        }catch (Exception e){
-//            throw new FileStorageException("Directory untuk upload gagal dibuat");
-//        }
-//    }
-
     //  Menyimpan file ke local server
     public String storeFile(File uploadRootDir, String fileNameTarget, MultipartFile fileData){
         String fileName = fileNameTarget;
 
         try{
-//           Path targetLocation = this.fileStorageLocation.resolve(fileName);
-//            byte[] bytes = file.getBytes();
-//            Path targetLocation = Paths.get(UPLOADED_FOLDER + fileName);
-//            Path targetLocation = getFilePath(fileName);
-//           Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
-//            Files.write(targetLocation, bytes);
             File serverFile = new File(uploadRootDir.getAbsolutePath() + "/"+ fileName);
             System.out.println(serverFile.getAbsoluteFile());
 
@@ -62,18 +39,10 @@ public class FileStorageService {
         }
     }
 
-    // Mengambil file path
-    public Path getFilePath(String fileName){
-//        return this.fileStorageLocation.resolve(fileName).normalize();
-        return Paths.get(UPLOADED_FOLDER + fileName);
-    }
 
     // Mengakses file dari local server
     public Resource loadFileAsResource(String filePath, String fileName){
         try{
-//            Path filePath = getFilePath(fileName);
-//            Path filePath = Paths.get(UPLOADED_FOLDER + fileName);
-//            Resource resource = new UrlResource(filePath.toUri());
             Resource resource = new UrlResource(new URI(filePath));
             if(resource.exists()){
                 return resource;
