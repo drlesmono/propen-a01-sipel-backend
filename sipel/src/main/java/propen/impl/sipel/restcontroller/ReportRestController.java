5b73e13dbbea97f913ceb858d69245d8cebcb1dd
Dennis Lesmono <drlesmono627@gmail.com>
2021-08-17 18:02:22 +0700

    changed private to public at reports/all again

--- sipel/src/main/java/propen/impl/sipel/restcontroller/ReportRestController.java
+++ sipel/src/main/java/propen/impl/sipel/restcontroller/ReportRestController.java
@@ -215,7 +215,7 @@ public class ReportRestController {
 
     @GetMapping(value="/api/v1/reports/all")
     @PreAuthorize("hasRole('MANAGER') or hasRole('ADMIN')")
-    private List<ReportModel> retrieveListReportAll(){
+    public List<ReportModel> retrieveListReportAll(){
         List<ReportModel> listReport = reportRestService.retrieveListReport();
         List<ReportModel> listReport2 = new ArrayList<>();
         for(int i= 0; i< listReport.size(); i++){
